d0f6a5913294c95415a5f917aa88561e91912b6c
MilaVEL <91512517+MilaVEL@users.noreply.github.com>
2024-10-08 19:22:09 +0800

    Token app.js

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-// подключение express
-const express = require("express");
-// создаем объект приложения
-const app = express();
-// определяем обработчик для маршрута "/"
-app.get("/", function (request, response) {
+const token = require('./token.js'); // Импортируем токен
+const TelegramBot = require('node-telegram-bot-api');
 
-    // отправляем ответ
-    response.send("<h2>Привет, Октагон!</h2>");
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // Проверяем, является ли это первым сообщением от пользователя
+    if (msg.text === '/start') {
+        bot.sendMessage(chatId, 'Привет, Октагон!');
+    }
 });
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    bot.sendMessage(chatId, resp);
+});
