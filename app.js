5e0a02becca017b23efb42db4bc730e5cbc7739d
MilaVEL <91512517+MilaVEL@users.noreply.github.com>
2024-10-08 12:37:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,23 @@ app.get("/", function (request, response) {
     // отправляем ответ
     response.send("<h2>Привет, Октагон!</h2>");
 });
+
+app.get("/static", function (request, response) {
+
+    response.send("<h2>header: “Hello” <br> body: “Octagon NodeJS Test</h2>");
+});
+
+app.use("/dynamic", function (request, response) {
+
+    var a = request.query.a;
+    var b = request.query.b;
+    var c = request.query.c;
+    var body = (a * b * c) / 3;
+    if (body) {
+        response.send(`<h1>header: Calculated </h1><p> body: ${body}</p>`);
+    } else {
+        response.send(`<h1>header: Error</h1>`);
+    }
+});
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
