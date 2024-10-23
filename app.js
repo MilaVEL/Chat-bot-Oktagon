1630a0164e9faee957531ac99a5000dc4e8a4090
MilaVEL <91512517+MilaVEL@users.noreply.github.com>
2024-10-23 19:16:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,17 @@ app.get("/static", function (request, response) {
 app.use("/dynamic", function (request, response) {
     
     const { a, b, c } = request.query;
-    const body = (a * b * c) / 3;
-    response.send(body ? `<b>header: Calculated</b><p>body: ${body}</p>` : `<b>header: Error</b>`);
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+        response.send("<b>header: Error</b><p>Body: One or more parameters are not numbers.</p>");
+        return;
+    }
+    if (a <= 0 || b <= 0 || c <= 0) {
+        response.send("<b>header: Error</b><p>Body: One or more parameters must be positive numbers.</p>");
+        return;
+    }
 
+    const body = (a * b * c) / 3;
+    response.send(`<b>header: Calculated</b><p>body: ${body}</p>`);
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
